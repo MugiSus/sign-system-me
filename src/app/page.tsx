613c399458c767274fbdb521277e38fe134093e7
MugiSus <mmgiss256@gmail.com>
2024-12-07 06:41:02 +0900

    generate og

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,21 @@
 import { Metadata } from "next";
 import Demo from "./demo";
 
-export async function generateMetadata(): Promise<Metadata> {
+export async function generateMetadata({
+  searchParams,
+}: {
+  searchParams: Promise<{ t: string }>;
+}): Promise<Metadata> {
+  const paramsText = (await searchParams).t || "";
+  const encodedText = encodeURIComponent(paramsText);
+
   return {
     title: "Sign System Me",
     description: "Love metro signs",
     openGraph: {
       images: [
         {
-          url: "/api/og",
+          url: paramsText ? `/api/og?text=${encodedText}` : "/api/og",
           width: 1200,
           height: 630,
           alt: "Sign System Me",
