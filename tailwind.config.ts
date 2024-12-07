5cf781c487574bac2b98e2c6c6b1e17e568210d7
MugiSus <mmgiss256@gmail.com>
2024-12-07 21:08:42 +0900

    slightly longer animation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,9 +13,9 @@ export default {
         foreground: "var(--foreground)",
       },
       animation: {
-        "slide-in-r": "slide-in-r 100ms cubic-bezier(0.15, 0.75, 0.4, 1)",
-        "slide-in-rb": "slide-in-rb 100ms cubic-bezier(0.15, 0.75, 0.4, 1)",
-        "slide-in-b": "slide-in-b 100ms cubic-bezier(0.15, 0.75, 0.4, 1)",
+        "slide-in-r": "slide-in-r 150ms cubic-bezier(0.15, 0.75, 0.4, 1)",
+        "slide-in-rb": "slide-in-rb 150ms cubic-bezier(0.15, 0.75, 0.4, 1)",
+        "slide-in-b": "slide-in-b 150ms cubic-bezier(0.15, 0.75, 0.4, 1)",
       },
       keyframes: {
         "slide-in-r": {
